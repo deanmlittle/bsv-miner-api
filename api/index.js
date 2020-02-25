7b2a52db659a5406dd3ddeb400b149cd602e90ca
deanmlittle <10921578+deanmlittle@users.noreply.github.com>
2020-02-25 14:57:53 +0000

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router(),
+apikey = require("../middlewares/apikey"),
+controller = require('./controller');
+
+router.get('/info', apikey, controller.getInfo);
+router.get('/fee', controller.getFee);
+router.get('/mempool', apikey, controller.getMempool);
+router.get('/tx/:txid', apikey, controller.getTX);
+router.post('/tx', apikey, controller.sendTX);
+router.get('/header/:id', controller.getBlockHeader);
+router.get('/block/:id', apikey, controller.getBlock);
+
+module.exports = router;
